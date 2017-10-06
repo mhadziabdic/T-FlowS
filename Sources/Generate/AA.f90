fd1354c2752f79afb52c3ea59c16c9ca1f94f2b7
Bojan Niceno <bojan@niceno.ch>
2017-10-06 21:10:17 +0200

    Removed unused files.

--- Sources/Generate/AA.f90
+++
@@ -1,5 +0,0 @@
-!======================================================================!
-include '../Library/allp_mod.f90'
-include '../Library/all_mod.f90'
-include '../Library/gen_mod.f90'
-include '../Library/par_mod.f90'
--- Sources/Process/AA.f90
+++
@@ -1,8 +0,0 @@
-!======================================================================!
-include '../Library/allp_mod.h90'
-include '../Library/all_mod.h90'
-include '../Library/pro_mod.h90'
-include '../Library/sol_mod.h90'
-include '../Library/les_mod.h90'
-include '../Library/par_mod.h90'
-include '../Library/rans_mod.h90'

f45ac1de9bde7bb73cd96bd248f3387a924cd9c7
Bojan Niceno <bojan@niceno.ch>
2017-10-11 17:21:44 +0200

    Renamed.

--- Sources/Shared/Save_Eps_Cuts.f90
+++ Sources/Shared/Save_Eps_Cuts.f90
@@ -1,25 +1,21 @@
-!======================================================================!
-  subroutine EpsSav(xg,yg,zg,sidegDx,sidegDy,dir)
-!----------------------------------------------------------------------!
-! Writes: Grid in encapsulated postscript format.                      !
-! ~~~~~~~                                                              !
-!------------------------------[Modules]-------------------------------!
+!==============================================================================!
+  subroutine Save_Eps_Cuts(xg,yg,zg,sidegDx,sidegDy,dir)
+!------------------------------------------------------------------------------!
+! Writes: Grid in encapsulated postscript format.                              !
+!----------------------------------[Modules]-----------------------------------!
   use all_mod
   use gen_mod
-!----------------------------------------------------------------------! 
+!------------------------------------------------------------------------------! 
   implicit none
-!-----------------------------[Parameters]-----------------------------!
+!---------------------------------[Parameters]---------------------------------!
   real      :: xg(MAXN),yg(MAXN),zg(MAXN),sidegDx(MAXS),sidegDy(MAXS)
   character :: dir
-!-------------------------------[Locals]-------------------------------!
-  integer   :: s, c1, c2, count, lw
-  character :: namEps*80
-  real      :: sclf, sclp, xmax,xmin,ymax,ymin,zmax,zmin, z0, fc
-  real      :: x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4,xin(4),yin(4) 
-!======================================================================!
-!   Note: singnificantly improved. Keep the old version allways at     !
-!         hand.                                                        !
-!----------------------------------------------------------------------!
+!-----------------------------------[Locals]-----------------------------------!
+  integer           :: s, c1, c2, count, lw
+  character(len=80) :: name_eps
+  real              :: sclf, sclp, xmax,xmin,ymax,ymin,zmax,zmin, z0, fc
+  real              :: x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4,xin(4),yin(4) 
+!==============================================================================!
 
   write(*,'(A38)')       '#-------------------------------------'
   write(*,'(A9,A1,A28)') '# Making ', dir, '.eps cut through the domain'
@@ -31,13 +27,15 @@
   if(z0 == 0) return
   write(*,*) 'Z0 = ', z0
 
-!<<<<<<<<<<<<<<<<<<<<<<<<<!
-!     create EPS file     !
-!<<<<<<<<<<<<<<<<<<<<<<<<<!
-  namEps = name
-  namEps(len_trim(name)+1:len_trim(name)+6) = '. .eps'
-  namEps(len_trim(name)+2:len_trim(name)+2) = dir
-  write(6, *) 'Now creating the file:', namEps
+  !----------------------!
+  !                      !
+  !   Create .eps file   !
+  !                      !
+  !----------------------!
+  name_eps = name
+  name_eps(len_trim(name)+1:len_trim(name)+6) = '. .eps'
+  name_eps(len_trim(name)+2:len_trim(name)+2) = dir
+  write(6, *) 'Now creating the file:', name_eps
 
   xmax=maxval(xg(1:NN)) 
   ymax=maxval(yg(1:NN)) 
@@ -49,7 +47,7 @@
   sclf = 100000.0/max((xmax-xmin),(ymax-ymin))
   sclp = 0.005 
 
-  open(9, FILE=namEps)
+  open(9, FILE=name_eps)
 
   write(9, '(A24)') '%!PS-Adobe-2.0 EPSF-1.2 '
   write(9, '(A24)') '%% Created by:  TFlowS %%'
@@ -95,44 +93,44 @@
     z4 = zg(SideN(s,4))
 
     count=0
-! 1-2
+    ! 1-2
     if(z1<=z0.and.z2>=z0 .or. z1>=z0 .and. z2<=z0) then 
       count=count+1
       fc = (z2-z0)/(z2-z1+TINY)
       xin(count) = fc*x1+(1.0-fc)*x2
       yin(count) = fc*y1+(1.0-fc)*y2
     end if
-! 1-3
+    ! 1-3
     if(z1<=z0 .and. z3>=z0 .or. z1>=z0 .and. z3<=z0) then 
       count=count+1
       fc = (z3-z0)/(z3-z1+TINY)
       xin(count) = fc*x1+(1.0-fc)*x3
       yin(count) = fc*y1+(1.0-fc)*y3
     end if
-! 2-3
+    ! 2-3
     if(z2<=z0 .and. z3>=z0 .or. z2>=z0 .and. z3<=z0) then 
       count=count+1
       fc = (z3-z0)/(z3-z2+TINY)
       xin(count) = fc*x2+(1.0-fc)*x3
       yin(count) = fc*y2+(1.0-fc)*y3
     end if
-!---- for quadrilateral faces
+    ! For quadrilateral faces
     if(SideN(s,0)==4) then
-! 1-4
+      ! 1-4
       if(z1<=z0 .and. z4>=z0 .or. z1>=z0 .and. z4<=z0) then 
         count=count+1
         fc = (z4-z0)/(z4-z1+TINY)
         xin(count) = fc*x1+(1.0-fc)*x4
         yin(count) = fc*y1+(1.0-fc)*y4
       end if
-! 2-4
+      ! 2-4
       if(z2<=z0 .and. z4>=z0 .or. z2>=z0 .and. z4<=z0) then 
         count=count+1
         fc = (z4-z0)/(z4-z2+TINY)
         xin(count) = fc*x2+(1.0-fc)*x4
         yin(count) = fc*y2+(1.0-fc)*y4
       end if
-! 3-4
+      ! 3-4
       if(z3<=z0 .and. z4>=z0 .or. z3>=z0 .and. z4<=z0) then 
         count=count+1
         fc = (z4-z0)/(z4-z3+TINY)
@@ -141,46 +139,46 @@
       end if
     end if
 
-!----- line width 
+    ! Line width 
     lw = 0 
 
-!----- if you want to move the domain, do something like:
-!----- xin = (1.0 - xin) 
+    ! If you want to move the domain, do something like:
+    ! xin = (1.0 - xin) 
 
     if(count == 2) then
       write(9,'(A6,I2,A5,2I8,A3,2I8,A3,A8)')                        &
-		'gs np ',lw,' slw ',                                &
-		int(sclf*xin(1)),int(sclf*yin(1)), ' m ',           &
-		int(sclf*xin(2)),int(sclf*yin(2)), ' l ',           &
-		' cp s gr' 
+                'gs np ',lw,' slw ',                                &
+                int(sclf*xin(1)),int(sclf*yin(1)), ' m ',           &
+                int(sclf*xin(2)),int(sclf*yin(2)), ' l ',           &
+                ' cp s gr' 
       if( Dx(s) /= 0.0 .or. Dy(s) /= 0.0 ) then
-	write(9,'(A6,I2,A5,2I8,A3,2I8,A3,A8)')                      &
-		  'gs np ',lw,' slw ',                              &
-		  int(sclf*(xin(1)-sidegDx(s))),                    &
-		  int(sclf*(yin(1)-sidegDy(s))), ' m ',             &
-		  int(sclf*(xin(2)-sidegDx(s))),                    &
-		  int(sclf*(yin(2)-sidegDy(s))), ' l ',             &
-		  ' cp s gr' 
+        write(9,'(A6,I2,A5,2I8,A3,2I8,A3,A8)')                      &
+                  'gs np ',lw,' slw ',                              &
+                  int(sclf*(xin(1)-sidegDx(s))),                    &
+                  int(sclf*(yin(1)-sidegDy(s))), ' m ',             &
+                  int(sclf*(xin(2)-sidegDx(s))),                    &
+                  int(sclf*(yin(2)-sidegDy(s))), ' l ',             &
+                  ' cp s gr' 
       end if
     end if
 
     if(count == 3) then
       write(9,'(A6,I2,A5,2I8,A3,2I8,A3,2I8,A3,A8)')                 &
-		'gs np ',lw,' slw ',                                &
-		int(sclf*xin(1)),int(sclf*yin(1)), ' m ',           &
-		int(sclf*xin(2)),int(sclf*yin(2)), ' l ',           &
-		int(sclf*xin(3)),int(sclf*yin(3)), ' l ',           &
-		' cp s gr' 
+                'gs np ',lw,' slw ',                                &
+                int(sclf*xin(1)),int(sclf*yin(1)), ' m ',           &
+                int(sclf*xin(2)),int(sclf*yin(2)), ' l ',           &
+                int(sclf*xin(3)),int(sclf*yin(3)), ' l ',           &
+                ' cp s gr' 
     end if
 
     if(count == 4) then
       write(9,'(A6,I2,A5,2I8,A3,2I8,A3,2I8,A3,2I8,A3,A8)')          &
-		'gs np ',lw,' slw ',                                &
-		int(sclf*xin(1)),int(sclf*yin(1)), ' m ',           &
-		int(sclf*xin(2)),int(sclf*yin(2)), ' l ',           &
-		int(sclf*xin(3)),int(sclf*yin(3)), ' l ',           &
-		int(sclf*xin(4)),int(sclf*yin(4)), ' l ',           &
-		' cp s gr' 
+                'gs np ',lw,' slw ',                                &
+                int(sclf*xin(1)),int(sclf*yin(1)), ' m ',           &
+                int(sclf*xin(2)),int(sclf*yin(2)), ' l ',           &
+                int(sclf*xin(3)),int(sclf*yin(3)), ' l ',           &
+                int(sclf*xin(4)),int(sclf*yin(4)), ' l ',           &
+                ' cp s gr' 
     end if
 
   end do 
@@ -189,4 +187,4 @@
 
   close(9)
 
-  end subroutine EpsSav
+  end subroutine Save_Eps_Cuts

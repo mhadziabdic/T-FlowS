b1a96cf3e123ca93b5b3d2647b11875ae82d58f8
Niceno Bojan <niceno@merlin-l-01.psi.ch>
2017-10-27 06:11:45 +0200

    Improved comments a bit.

--- Sources/Generate/Print_Grid_Statistics.f90
+++ Sources/Generate/Print_Grid_Statistics.f90
@@ -13,14 +13,14 @@
   integer :: i, j, k, numb, nonz, stencw
 !==============================================================================!
 
-  write(*,*) '#================================='
+  write(*,*) '#==============================================================='
   write(*,*) '# Grid statistics'
-  write(*,*) '#---------------------------------'
-  write(*,*) '# number of nodes         :', NN
-  write(*,*) '# number of cells         :', NC
-  write(*,*) '# number of sides         :', NS
-  write(*,*) '# number of boundary cells:', NbC
-  write(*,*) '#---------------------------------'
+  write(*,*) '#---------------------------------------------------------------'
+  write(*,*) '# Number of nodes         :', NN
+  write(*,*) '# Number of cells         :', NC
+  write(*,*) '# Number of sides         :', NS
+  write(*,*) '# Number of boundary cells:', NbC
+  write(*,*) '#---------------------------------------------------------------'
 
   ! Find the number of non zero entries
   nonz=0
@@ -32,11 +32,11 @@
     nonz = nonz + stencw
   end do
 
-  write(*,*) '# number of non zero matrix entries:', nonz
-  write(*,*) '# average stencil size:', real(nonz)/real(NC)
-  write(*,*) '# max number of nodes and cells:',   MAXN
-  write(*,*) '# max number of boundary cells:',    MAXB
-  write(*,*) '#---------------------------------'
+  write(*,*) '# Number of non zero matrix entries:', nonz
+  write(*,*) '# Average stencil size:', real(nonz)/real(NC)
+  write(*,*) '# Max number of nodes and cells:',   MAXN
+  write(*,*) '# Max number of boundary cells:',    MAXB
+  write(*,*) '#---------------------------------------------------------------'
 
   ! Neighbours
   do j=1,24
@@ -49,7 +49,7 @@
       if(stencw  ==  j) numb=numb+1
     end do
     if(numb /= 0) then
-      write(*,*) '# number of cells with ',j, ' neighbours: ',numb
+      write(*,*) '# Number of cells with ',j, ' neighbours: ',numb
     endif
   end do
 
@@ -60,10 +60,10 @@
       if(TwinN(i,0)  ==  j) numb=numb+1
     end do
     if(numb /= 0) then
-      write(*,*) '# number of nodes with ',j, ' twins: ',numb
+      write(*,*) '# Number of nodes with ',j, ' twins: ',numb
     endif 
   end do
 
-  write(*,*) '#---------------------------------'
+  write(*,*) '#---------------------------------------------------------------'
 
   end subroutine Print_Grid_Statistics

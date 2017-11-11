0ce5b4b6f10411435ee208bb58f6a48fe7221dff
mhadziabdic <muhamed.hadziabdic@gmail.com>
2017-11-11 20:37:15 +0100

    CalcVISt_EBM is renamed to CalcVISt_RSM as it is more general name.

---
+++ Sources/Process/CalcVISt_RSM.f90
@@ -0,0 +1,54 @@
+!======================================================================!
+  subroutine CalcVISt_RSM() 
+!----------------------------------------------------------------------!
+!   Computes the turbulent viscosity for RSM models (EBM and HJ).      !
+!   If hybrid option is used turbulent diffusivity is modeled by VISt.
+!   Otherwise, VISt is used as false diffusion in order to increase 
+!   stability of computation.
+!----------------------------------------------------------------------!
+!------------------------------[Modules]-------------------------------!
+  use all_mod
+  use pro_mod
+  use les_mod
+  use rans_mod
+!----------------------------------------------------------------------!
+  implicit none
+!------------------------------[Calling]-------------------------------!
+!-------------------------------[Locals]-------------------------------!
+  integer :: c, c1, c2, s
+  real    :: CK, Yplus, Fmu, Ret, yStar, Cmu_mod                                        
+!======================================================================!
+
+  Ret   = 0.0
+  Fmu   = 0.0 
+  yPlus = 0.0 
+  call CalcShear(U % n, V % n, W % n, Shear)
+
+  if(SIMULA == HJ) then
+    do c=1,NC
+      Kin % n(c) = 0.5*max(uu%n(c)+vv%n(c)+ww%n(c),1.0e-12)
+
+      Cmu_mod = max(-(uu%n(c)*Ux(c)+vv%n(c)*Vy(c)+ww%n(c)*Wz(c)+&
+                  uv%n(c)*(Vx(c)+Uy(c))+uw%n(c)*(Uz(c)+&
+                  Wx(c))+vw%n(c)*(Vz(c)+Wy(c)))/max(Kin%n(c)*Kin%n(c)/Eps_tot(c)*Shear(c)*Shear(c),1.0e-12),0.0)
+
+      Cmu_mod = min(0.12,Cmu_mod) 
+      VISt(c) = Cmu_mod*DENc(material(c)) * Kin%n(c) * Kin%n(c) / Eps % n(c)
+    end do 
+  else if(SIMULA==EBM) then
+    do c=1,NC
+      Kin % n(c) = 0.5*max(uu%n(c)+vv%n(c)+ww%n(c),1.0e-12)
+
+      Cmu_mod = max(-(uu%n(c)*Ux(c)+vv%n(c)*Vy(c)+ww%n(c)*Wz(c)+&
+                  uv%n(c)*(Vx(c)+Uy(c))+uw%n(c)*(Uz(c)+&
+                  Wx(c))+vw%n(c)*(Vz(c)+Wy(c)))/max(Kin%n(c)*Kin%n(c)/Eps%n(c)*Shear(c)*Shear(c),1.0e-12),0.0)
+
+      Cmu_mod = min(0.12,Cmu_mod) 
+      VISt(c) = Cmu_mod*DENc(material(c)) * Kin%n(c) * Kin%n(c) / Eps % n(c)
+    end do
+  end if
+
+  call Exchng(VISt)  
+  RETURN
+
+  end subroutine CalcVISt_RSM

22dc1cc1cce087369e4a1ab052680c1e1147d75e
Bojan Niceno <bojan@niceno.ch>
2018-01-17 10:19:18 +0100

    Many changes towards re-introduction of makefiles, and connected to that, introduction of CGNS file format. (No CGNS support with this commit though).

--- Sources/Process/User/User_Allocate.f90
+++
@@ -1,2 +0,0 @@
-!   This is embrio of a function which will be used to allocate   
-!   memory for user-defined variables.                         
--- Sources/Process/User/User_Mod.f90
+++
@@ -1,2 +0,0 @@
-!   This is embrio of a future User module, a place where user can
-!   define his/her variables and pass them around his functions

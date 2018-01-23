2d88d8ba6869e9c293067bf9e87e6f4ac8863c53
Niceno <bojan@niceno.ch>
2018-01-23 10:39:53 +0100

    File .pvtu is now created from function "Save_Vtu_Cells".

--- Sources/Shared/Save_Vtu_Cells.f90
+++ Sources/Shared/Save_Vtu_Cells.f90
@@ -5,6 +5,7 @@
 !----------------------------------[Modules]-----------------------------------!
   use all_mod, only: material
   use gen_mod, only: NewN, NewC
+  use div_mod, only: n_sub
   use Grid_Mod
 !------------------------------------------------------------------------------!
   implicit none
@@ -14,6 +15,7 @@
 !-----------------------------------[Locals]-----------------------------------!
   integer            :: c, n, offset
   character(len=80)  :: name_out
+!------------------------------[Local parameters]------------------------------!
   integer, parameter :: VTK_TETRA      = 10
   integer, parameter :: VTK_HEXAHEDRON = 12
   integer, parameter :: VTK_WEDGE      = 13
@@ -62,24 +64,24 @@
   do c = 1, grid % n_cells
     if(NewC(c) /= 0) then
       if(grid % cells_n_nodes(c) == 8) then
-        write(9,'(8I9)')                                          &
-          NewN(grid % cells_n(1,c))-1, NewN(grid % cells_n(2,c))-1,   &
-          NewN(grid % cells_n(4,c))-1, NewN(grid % cells_n(3,c))-1,   &
-          NewN(grid % cells_n(5,c))-1, NewN(grid % cells_n(6,c))-1,   &
+        write(9,'(8I9)')                                             &
+          NewN(grid % cells_n(1,c))-1, NewN(grid % cells_n(2,c))-1,  &
+          NewN(grid % cells_n(4,c))-1, NewN(grid % cells_n(3,c))-1,  &
+          NewN(grid % cells_n(5,c))-1, NewN(grid % cells_n(6,c))-1,  &
           NewN(grid % cells_n(8,c))-1, NewN(grid % cells_n(7,c))-1
       else if(grid % cells_n_nodes(c) == 6) then
-        write(9,'(6I9)')                                          &
-          NewN(grid % cells_n(1,c))-1, NewN(grid % cells_n(2,c))-1,   &
-          NewN(grid % cells_n(3,c))-1, NewN(grid % cells_n(4,c))-1,   &
+        write(9,'(6I9)')                                             &
+          NewN(grid % cells_n(1,c))-1, NewN(grid % cells_n(2,c))-1,  &
+          NewN(grid % cells_n(3,c))-1, NewN(grid % cells_n(4,c))-1,  &
           NewN(grid % cells_n(5,c))-1, NewN(grid % cells_n(6,c))-1
       else if(grid % cells_n_nodes(c) == 4) then
-        write(9,'(4I9)')                                          &
-          NewN(grid % cells_n(1,c))-1, NewN(grid % cells_n(2,c))-1,   &
+        write(9,'(4I9)')                                             &
+          NewN(grid % cells_n(1,c))-1, NewN(grid % cells_n(2,c))-1,  &
           NewN(grid % cells_n(3,c))-1, NewN(grid % cells_n(4,c))-1
       else if(grid % cells_n_nodes(c) == 5) then
-        write(9,'(5I9)')                                          &
-          NewN(grid % cells_n(5,c))-1, NewN(grid % cells_n(1,c))-1,   &
-          NewN(grid % cells_n(2,c))-1, NewN(grid % cells_n(4,c))-1,   &
+        write(9,'(5I9)')                                             &
+          NewN(grid % cells_n(5,c))-1, NewN(grid % cells_n(1,c))-1,  &
+          NewN(grid % cells_n(2,c))-1, NewN(grid % cells_n(4,c))-1,  &
           NewN(grid % cells_n(3,c))-1
       else
         print *, '# Unsupported cell type with ',  &
@@ -139,5 +141,47 @@
   write(9,'(a)') '</VTKFile>'
 
   close(9)
+
+  !-----------------------!
+  !                       !
+  !   Create .pvtu file   !
+  !                       !
+  !-----------------------!
+
+  ! Create it only from subdomain 1, when decomposed
+  if(n_sub > 1 .and. sub == 1) then
+
+    call Name_File(0, name_out, '.pvtu')
+    print *, '# Creating the file: ', trim(name_out)
+    open(9, file = name_out)
+  
+    ! Header
+    write(9,'(a)') '<?xml version="1.0"?>'
+    write(9,'(a)') '<VTKFile type="PUnstructuredGrid">'
+    write(9,'(a)') '<PUnstructuredGrid GhostLevel="0">'
+
+    ! This section must be present
+    write(9,'(a)') '<PPoints>'
+    write(9,'(a)') '<PDataArray type="Float32" NumberOfComponents="3" format="ascii"/>'
+    write(9,'(a)') '</PPoints>'
+
+    ! Data section is not mandatory, but very useful
+    write(9,'(a)') '<PCellData Scalars="scalars" vectors="velocity">'
+    write(9,'(a)') '<PDataArray type="UInt8" Name="materials" format="ascii"/>'
+    write(9,'(a)') '</PCellData>'
+
+    ! Write out the names of all the pieces
+    do n = 1, n_sub
+      call Name_File(n, name_out, '.vtu')
+      write(9, '(a,a,a)') '<Piece Source="', trim(name_out), '"/>'
+    end do
+
+    ! Footer
+    write(9, '(a)'), '</PUnstructuredGrid>'
+    write(9, '(a)'), '</VTKFile>'
+
+    close(9)
+
+  end if
 
   end subroutine

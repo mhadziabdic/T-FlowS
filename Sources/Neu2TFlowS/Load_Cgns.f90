e49f6af12a3fc0b3bfd2b1c392fd40ada8d926b1
Egor Palkin <palkinev89@gmail.com>
2018-01-24 22:25:41 +0100

    Now section block correctly reads all basic 3d cell types and ZoneBC node. ZoneBC node has info on normal vector, b.c. type (Neiman, Dirichlet, inflow and etc.) But face structure is in Elements block.

--- Sources/Neu2TFlowS/Load_Cgns.f90
+++ Sources/Neu2TFlowS/Load_Cgns.f90
@@ -25,7 +25,7 @@
   integer                 :: c, n, dir
   integer                 :: file_idx, base_idx, zone_idx, sect_idx, ier
   integer                 :: n_sect, n_bnd
-  integer                 :: first_cell, last_cell, itype, iparent_data, iparent_flag
+  integer                 :: cell_type, iparent_data, iparent_flag
 
   character(len=80)       :: zone_name, name_sect
   character*32            :: base_name
@@ -44,12 +44,32 @@
   integer                 :: coord_idx
   integer                 :: last_node
   integer                 :: first_node
-  integer                 :: mesh_info(3)
-  real*4, allocatable     :: data_single(:)
-  real*8, allocatable     :: data_double(:)
+
+  ! *8
+  integer               :: first_cell
+  integer               :: last_cell
+  integer               :: mesh_info(3)
+
+  real*4,  allocatable    :: data_single(:)
+  real*8,  allocatable    :: data_double(:)
+  integer, allocatable    :: data_integer(:)
+  integer, allocatable :: hex_connections(:, :)
+  integer, allocatable :: pyramid_connections(:, :)
+  integer, allocatable :: prism_connections(:, :)
+  integer, allocatable :: tetra_connections(:, :)
+  integer, allocatable :: cell_connections(:, :)
+  
+  character*32 boconame
+  integer               :: bocotype, bc_idx
+  integer ndataset
+  integer  NormalListFlag
+  integer ptset_type
+  integer  npnts
+  integer NormalIndex(3)
 !==============================================================================!
 
   name_in = problem_name
+
   name_in(len_trim(problem_name)+1:len_trim(problem_name)+5) = ".cgns"
 
   !Open a CGNS file 
@@ -62,6 +82,8 @@
       write(*,"(A,A)") "# FAILED to read the file: ", trim(name_in)
       call Cg_Error_Exit_f()
     endif
+  
+  !call cg_version_f(fn, version, ier)    
 
   ! Get number of CGNS base nodes in file 
   call Cg_Nbases_F(file_idx, & ! CGNS file index number
@@ -122,11 +144,11 @@
           write(*,"(A)") "#     Failed read zone info"
           call Cg_Error_Exit_f()
         endif
-      write(*,"(A,I3)") "#     Zone index: ",                zone_idx
-      write(*,"(A,A)")  "#     Zone name: ",                 zone_name
-      write(*,"(A,I9)") "#     Nodes: ",                     mesh_info(1)
-      write(*,"(A,I9)") "#     Cells: ",                     mesh_info(2)
-      write(*,"(A,I9)") "#     Boundary nodes(if sorted): ", mesh_info(3)
+      write(*,"(A,I3)")  "#     Zone index: ",                zone_idx
+      write(*,"(A,A)")   "#     Zone name: ",                 zone_name
+      write(*,"(A,I16)") "#     Nodes: ",                     mesh_info(1)
+      write(*,"(A,I16)") "#     Cells: ",                     mesh_info(2)
+      write(*,"(A,I16)") "#     Boundary nodes(if sorted): ", mesh_info(3)
 
       n_nodes = n_nodes + mesh_info(1)
       n_cells = n_cells + mesh_info(2)
@@ -248,7 +270,6 @@
 
       ! Browse through all sections to read elements
       elements_connection: do sect_idx = 1, n_sect
-        
 
         ! Get info for an element section
         ! recieves name_sect, first_cell: last_cell, n_bnd and cell_type
@@ -257,7 +278,7 @@
                                zone_idx,     & ! Zone index number
                                sect_idx,     & ! Element section index
                                name_sect,    & ! Name of the Elements_t node
-                               itype,        & ! Type of element
+                               cell_type,    & ! Type of element
                                first_cell,   & ! Index of first element
                                last_cell,    & ! Index of last element
                                n_bnd,        & ! Index of last boundary element
@@ -265,22 +286,146 @@
                                ier)            ! Error status
 
         write(*,"(A,A)")  "#         section name: ", name_sect
-        write(*,"(A,A)")  "#         section type: ", ElementTypeName(itype)
+        write(*,"(A,A)")  "#         section type: ", ElementTypeName(cell_type)
         write(*,"(A,I9)") "#         first idx:",     first_cell
         write(*,"(A,I9)") "#         last idx:",      last_cell             
 
-        ! Read element data 
-        call Cg_Elements_Read_F(file_idx,       & ! CGNS file index number
-                                base_idx,       & ! Base index number
-                                zone_idx,       & ! Zone index number
-                                sect_idx,       & ! Element section index
-                                grid % cells_n, & ! Element connectivity data
-                                iparent_data,   & ! For boundary or interface 
-                                ier)              ! Error status
+        ! read cell_connections
+        if     (ElementTypeName(cell_type) .eq. 'HEXA_8') then
+          allocate(hex_connections(1:8, first_cell:last_cell))
+          hex_connections = 0
+
+          ! Read element data(HEXA_8)
+          call Cg_Elements_Read_F(file_idx,         & ! CGNS file index number
+                                  base_idx,         & ! Base index number
+                                  zone_idx,         & ! Zone index number
+                                  sect_idx,         & ! Element section index
+                                  hex_connections,  & ! Element connectivity data
+                                  iparent_data,     & ! For boundary or interface 
+                                  ier)                ! Error status
+
+          do i = first_cell, last_cell
+            write(*,"(8I9)") hex_connections(1:8,i)
+          end do
+        elseif (ElementTypeName(cell_type) .eq. 'PYRA_5') then
+          allocate(pyramid_connections(1:5, first_cell:last_cell))
+          pyramid_connections = 0
+
+          ! Read element data(PYRA_5)
+          call Cg_Elements_Read_F(file_idx,            & ! CGNS file index number
+                                  base_idx,            & ! Base index number
+                                  zone_idx,            & ! Zone index number
+                                  sect_idx,            & ! Element section index
+                                  pyramid_connections, & ! Element connectivity data
+                                  iparent_data,        & ! For boundary or interface 
+                                  ier)                   ! Error status
+          do i = first_cell, last_cell
+            write(*,"(5I9)") pyramid_connections(1:5,i)
+          end do
+        elseif (ElementTypeName(cell_type) .eq. 'PENTA_6') then
+          allocate(prism_connections(1:6, first_cell:last_cell))
+          prism_connections = 0
+
+          ! Read element data(PENTA_6)
+          call Cg_Elements_Read_F(file_idx,          & ! CGNS file index number
+                                  base_idx,          & ! Base index number
+                                  zone_idx,          & ! Zone index number
+                                  sect_idx,          & ! Element section index
+                                  prism_connections, & ! Element connectivity data
+                                  iparent_data,      & ! For boundary or interface 
+                                  ier)                   ! Error status
+
+          do i = first_cell, last_cell
+            write(*,"(6I9)") prism_connections(1:6,i)
+          end do
+        elseif (ElementTypeName(cell_type) .eq. 'TETRA_4') then
+          allocate(tetra_connections(1:8, first_cell:last_cell))
+          tetra_connections = 0
+
+          ! Read element data(TETRA_4)
+          call Cg_Elements_Read_F(file_idx,           & ! CGNS file index number
+                                  base_idx,           & ! Base index number
+                                  zone_idx,           & ! Zone index number
+                                  sect_idx,           & ! Element section index
+                                  tetra_connections,  & ! Element connectivity data
+                                  iparent_data,       & ! For boundary or interface 
+                                  ier)                  ! Error status
+          do i = first_cell, last_cell
+            write(*,"(4I9)") tetra_connections(1:4,i)
+          end do
+        end if
+        ! I can read BC here also !!
+        ! tell Bojan about this:
+        ! mesh info cell count accounts ONLY for internal cells
+        ! and for boundary cell cngs produces new cell idx (show in cgnsview)
 
         write(*,"(A)") "# ---------------------------"
 
+
       end do elements_connection
+    
+
+      !---------------!
+      !   B.C. block  !
+      !---------------!
+
+      ! Access a node via label/name, index pairs 
+      call cg_goto_f(file_idx,   & ! CGNS file index number
+                     base_idx,   & ! Base index number
+                     ier,        & ! Error status
+                     'Zone_t',   & ! node of zone type
+                     zone_idx,   & ! Zone index number
+                     'ZoneBC_t', & ! search for node "ZoneBC_t"
+                     1,          & ! ???
+                     'end')        ! indicates end of call
+      if (ier.ne.0) then
+        write(*,"(A)")"#     FAILED to navigate ro ZoneBC node"
+        call Cg_Error_Exit_f()
+      endif
+      
+      ! Get number of boundary condition in zone
+      call cg_nbocos_f(file_idx,  & ! CGNS file index number
+                       base_idx,  & ! Base index number
+                       zone_idx,  & ! Zone index number
+                       n_bound,   & ! Number of boundary conditions in zone
+                       ier)         ! Error status
+      if (ier.ne.0) then
+        write(*,"(A)")"#     FAILED to obtain number of b.c."
+        call Cg_Error_Exit_f()
+      endif
+
+      write(*,"(A,I3)") "#     Zone index: ",          zone_idx
+      write(*,"(A,I3)") "#     Boundary conditions: ", n_bound
+
+
+      boundary_conditions: do bc_idx = 1, n_bound
+        ! Get boundary condition info 
+        call cg_boco_info_f(file_idx,       & ! CGNS file index number
+                            base_idx,       & ! Base index number
+                            zone_idx,       & ! Zone index number
+                            bc_idx,         & ! Boundary condition index number
+                            boconame,       & ! Name of the boundary condition
+                            bocotype,       & ! Type of boundary condition
+                            ptset_type,     & ! The extent of the boundary condition
+                            npnts,          & ! Number of points/cells to make BC
+                            NormalIndex,    & ! Index vector normal direction of BC
+                            NormalListFlag, & ! Flag indicating if the normals are defined in NormalList 
+                            data_type,      & ! Data type used in the definition of the normals
+                            ndataset,       & ! Number of boundary condition datasets
+                            ier)              ! Error status
+        if (ier.ne.0) then
+          write(*,"(A)")"#     FAILED to obtain number of b.c."
+          call Cg_Error_Exit_f()
+        endif
+        write(*,"(A,I3)") "#       B.C. index: ",  bc_idx
+        write(*,"(A,A)")  "#       B.C. name: ",   boconame
+        write(*,"(A,A)")  "#       B.C. Extent: ", PointSetTypeName(ptset_type)
+        write(*,"(A,9I9)")"#       B.C. normal: ", NormalIndex(1), NormalIndex(2), NormalIndex(3)
+        write(*,"(A,5I5)")"#       B.C. normal list flag: ", NormalListFlag
+        write(*,"(A,A)")  "#       B.C. normal data_type: ", DataTypeName(data_type)
+
+        write(*,"(A)") "# ---------------------------"
+      end do boundary_conditions
 
     end do zones
 
@@ -304,3 +449,10 @@
 stop
 
   end subroutine
+!cg_nsections - Get number of element sections
+!cg_section_read - Get info for an element section
+!cg_ElementDataSize - Get size of element connectivity data array
+!cg_ElementPartialSize - Get size of element connectivity data array for partial read
+!cg_elements_read - Read element data
+!cg_elements_partial_read - Read subset of element data
+!cg_npe - Get number of nodes for an element type 

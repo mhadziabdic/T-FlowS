60dbbddc44b50f15145e867f162fc96660d978ee
Niceno <bojan@niceno.ch>
2018-02-09 18:07:36 +0100

    Added one additional check, not a big deal.

--- Sources/Convert/Grid_Topology.f90
+++ Sources/Convert/Grid_Topology.f90
@@ -36,7 +36,7 @@
         material(-grid % n_bnd_cells) = material(i)
 
         ! Faces
-        grid % n_faces  = grid % n_faces  + 1
+        grid % n_faces = grid % n_faces  + 1
         grid % faces_c(1,grid % n_faces) = i
         grid % faces_c(2,grid % n_faces) = -grid % n_bnd_cells
 
@@ -57,10 +57,9 @@
             grid % cells_n(3,-grid % n_bnd_cells)
           grid % faces_n(4,grid % n_faces) =  &
             grid % cells_n(4,-grid % n_bnd_cells)
-        end if
 
         ! Prisms:
-        if(grid % cells_n_nodes(i) == 6) then
+        else if(grid % cells_n_nodes(i) == 6) then
           if(j <= 3) then    ! faces (1), (2) and (3)
             grid % cells_n_nodes(-grid % n_bnd_cells) = 4 
             grid % cells_n(1,-grid % n_bnd_cells) = grid % cells_n(neu_wed(j,1),i)
@@ -91,10 +90,9 @@
             grid % faces_n(3,grid % n_faces) =  &
               grid % cells_n(3,-grid % n_bnd_cells)
           end if
-        end if
 
         ! Tetrahedra:
-        if(grid % cells_n_nodes(i) == 4) then
+        else if(grid % cells_n_nodes(i) == 4) then
           if(j <= 4) then
             grid % cells_n_nodes(-grid % n_bnd_cells) = 3 
             grid % cells_n(1,-grid % n_bnd_cells) = grid % cells_n(neu_tet(j,1),i)
@@ -109,10 +107,9 @@
             grid % faces_n(3,grid % n_faces) =  &
               grid % cells_n(3,-grid % n_bnd_cells)
           end if
-        end if
 
         ! Pyramides:
-        if(grid % cells_n_nodes(i) == 5) then
+        else if(grid % cells_n_nodes(i) == 5) then
           if(j == 1) then    ! face (1)
             grid % cells_n_nodes(-grid % n_bnd_cells) = 4
             grid % cells_n(1,-grid % n_bnd_cells) = grid % cells_n(neu_pyr(j,1),i)
@@ -143,6 +140,11 @@
             grid % faces_n(3,grid % n_faces) =  &
               grid % cells_n(3,-grid % n_bnd_cells)
           end if
+        else
+          print *, '# Cell with invalid number of nodes: ',  &
+                   grid % cells_n_nodes(i)
+          print *, '# Exiting!'
+          stop
         end if
 
       end if

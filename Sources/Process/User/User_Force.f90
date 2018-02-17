1ec464b50040880c49ae5a577a7adc99eab8ac19
mhadziabdic <muhamed.hadziabdic@gmail.com>
2018-02-17 18:32:37 +0100

    Buoyancu force is added.

--- Sources/Process/User/User_Force.f90
+++ Sources/Process/User/User_Force.f90
@@ -7,6 +7,12 @@
 !   vector ("b_vector") are sent.
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
+  use all_mod
+  use pro_mod
+  use rans_mod
+  use par_mod
+  use Info_Mod
+  use Parameters_Mod
   use Grid_Mod
   use Var_Mod
   use Matrix_Mod
@@ -23,7 +29,10 @@
   integer, allocatable :: working_i(:), arrays_i(:), come_i(:), here_i(:)
   real                 :: depends_r, on_r, the_r, case_r
   integer              :: depends_i, on_i, the_i, case_i
+  integer              :: c
+  real                 :: grav
 !------------------------------------------------------------------------------!
+
 
   !----------------------------------------------------! 
   !                                                    !
@@ -31,25 +40,31 @@
   !                                                    !
   !----------------------------------------------------! 
 
+  if(BUOY == YES) then
   !-------------------------------------------------------!
   !  Set source for velocity component in "x" direction   !
   !-------------------------------------------------------!
-  if( ui % name == 'U' ) then  
-
-  end if
+    if( ui % name == 'U' ) then  
+      grav = grav_x      
+    end if
   
   !-------------------------------------------------------!
-  !  Set source for velocity component in "x" direction   !
+  !  Set source for velocity component in "y" direction   !
   !-------------------------------------------------------!
-  if( ui % name == 'V' ) then  
-
-  end if
+    if( ui % name == 'V' ) then  
+      grav = grav_y      
+    end if
   
   !-------------------------------------------------------!
-  !  Set source for velocity component in "x" direction   !
+  !  Set source for velocity component in "z" direction   !
   !-------------------------------------------------------!
-  if( ui % name == 'W' ) then  
+    if( ui % name == 'W' ) then  
+      grav = grav_z      
+    end if
 
+    do c=1,grid % n_cells
+      b(c) = b(c) - grav*(T%n(c) - Tref)*grid % vol(c) 
+    end do
   end if
 
   end subroutine  ! fourth level comments

bf0df38638362f4cfffa128744a146339faf8077
Niceno <bojan@niceno.ch>
2018-05-01 14:27:20 +0200

    Added; first working version.

---
+++ Tests/Rans/Backstep_Re_28000/User_Mod/Save_Results.f90
@@ -0,0 +1,18 @@
+include '../User_Mod/Backstep_Profiles.f90'
+
+!==============================================================================!
+  subroutine User_Mod_Save_Results(grid, save_name) 
+!------------------------------------------------------------------------------!
+!   Calls User_Backstep_Profiles and User_Backstep_Cf_St functions.            !
+!------------------------------------------------------------------------------!
+  use Grid_Mod
+!------------------------------------------------------------------------------!
+  implicit none
+!---------------------------------[Arguments]----------------------------------!
+  type(Grid_Type)  :: grid
+  character(len=*) :: save_name
+!==============================================================================!
+
+  call User_Backstep_Profiles(grid, save_name)
+
+  end subroutine

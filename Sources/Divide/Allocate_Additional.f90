f38c4145ff7fc11e1a8445d7c32dbde4de373854
Niceno <bojan@niceno.ch>
2018-05-03 12:23:06 +0200

    Standardizing variable names.

--- Sources/Divide/Allocate_Additional.f90
+++ Sources/Divide/Allocate_Additional.f90
@@ -5,7 +5,7 @@
 !------------------------------------------------------------------------------!
 !----------------------------------[Modules]-----------------------------------!
   use gen_mod 
-  use div_mod
+  use Div_Mod
   use Grid_Mod
 !------------------------------------------------------------------------------!
   implicit none
@@ -17,20 +17,20 @@
 !==============================================================================!
 
   ! Variables declared in gen_mod.h90:
-  allocate (new_c(-grid % n_bnd_cells-1:grid % n_cells)); new_c = 0 
-  allocate (new_f( grid % n_faces));                      new_f = 0
-  allocate (new_n( grid % n_nodes));                      new_n = 0 
+  allocate (new_c(-grid % n_bnd_cells-1:grid % n_cells));  new_c = 0 
+  allocate (new_f( grid % n_faces));                       new_f = 0
+  allocate (new_n( grid % n_nodes));                       new_n = 0 
 
   ! Variables declared in div.h90:
-  allocate (ix(-grid % n_bnd_cells:grid % n_cells));  ix=0
-  allocate (iy(-grid % n_bnd_cells:grid % n_cells));  iy=0
-  allocate (iz(-grid % n_bnd_cells:grid % n_cells));  iz=0
-  allocate (iin(-grid % n_bnd_cells:grid % n_cells)); iin=0
-  allocate (criter(grid % n_cells));   criter=0
+  allocate (ix(-grid % n_bnd_cells:grid % n_cells));   ix  = 0
+  allocate (iy(-grid % n_bnd_cells:grid % n_cells));   iy  = 0
+  allocate (iz(-grid % n_bnd_cells:grid % n_cells));   iz  = 0
+  allocate (iin(-grid % n_bnd_cells:grid % n_cells));  iin = 0
+  allocate (criter(grid % n_cells));  criter=0
 
-  allocate (proces(grid % n_cells)); proces=0
-  allocate (BuSeIn(grid % n_faces)); BuSeIn=0
-  allocate (BuReIn(grid % n_faces)); BuReIn=0
-  allocate (BufPos(grid % n_faces)); BufPos=0
+  allocate (proces      (grid % n_cells));  proces       = 0
+  allocate (buf_send_ind(grid % n_faces));  buf_send_ind = 0
+  allocate (buf_recv_ind(grid % n_faces));  buf_recv_ind = 0
+  allocate (buf_pos     (grid % n_faces));  buf_pos      = 0
 
   end subroutine

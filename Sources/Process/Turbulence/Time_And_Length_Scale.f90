86f344a7c047fe5b404e8e5b7a034e5268e1be09
Niceno <bojan@niceno.ch>
2018-05-07 13:04:42 +0200

    Renamed some variables to bring them to coding standards we adopted.

--- Sources/Process/Turbulence/Time_And_Length_Scale.f90
+++ Sources/Process/Turbulence/Time_And_Length_Scale.f90
@@ -29,26 +29,26 @@
     if(ROUGH .eq. YES) then
       do c = 1, grid % n_cells 
         t1 = kin % n(c)/(eps % n(c) + TINY)
-        t2 = Ct*sqrt(kin_visc/eps % n(c))
+        t2 = c_t*sqrt(kin_visc/eps % n(c))
 
         l1 = kin % n(c)**1.5/eps % n(c)
         l2 = Cni*(kin_visc**3/eps % n(c))**0.25
 
-        Tsc(c) = max(t1,t2)
-        Lsc(c) = Cl*max(l1,l2)
+        t_scale(c) = max(t1,t2)
+        l_scale(c) = c_l*max(l1,l2)
       end do
     else
       do c = 1, grid % n_cells
         t1 = kin % n(c)/(eps % n(c) + TINY)
-        t2 = Ct*sqrt(kin_visc/eps % n(c))
-        t3 = 0.6/(sqrt(3.0)*CmuD * zeta % n(c) * Shear(c))
+        t2 = c_t*sqrt(kin_visc/eps % n(c))
+        t3 = 0.6/(sqrt(3.0)*c_mu_d * zeta % n(c) * shear(c))
 
         l1 = kin % n(c)**1.5/eps % n(c)
         l2 = Cni*(kin_visc**3/eps % n(c))**0.25
-        l3 = sqrt(kin % n(c)/3.0)/(CmuD * zeta % n(c) * Shear(c)) 
+        l3 = sqrt(kin % n(c)/3.0)/(c_mu_d * zeta % n(c) * shear(c)) 
 
-        Tsc(c) = max(min(t1,t3),t2)
-        Lsc(c) = Cl*max(min(l1,l3),l2)
+        t_scale(c) = max(min(t1,t3),t2)
+        l_scale(c) = c_l*max(min(l1,l3),l2)
       end do
     end if 
 
@@ -56,13 +56,13 @@
     do c = 1, grid % n_cells
       kin % n(c) = max(0.5*(uu % n(c) + vv % n(c) + ww % n(c)),1.0e-12)
       t1 = kin % n(c)/(eps % n(c))
-      t2 = Ct*sqrt(kin_visc/abs(eps % n(c)))
+      t2 = c_t*sqrt(kin_visc/abs(eps % n(c)))
 
       l1 = kin % n(c)**1.5/eps % n(c)
       l2 = Cni*(kin_visc**3/abs(eps % n(c)))**0.25
 
-      Tsc(c) = max(t1,t2)
-      Lsc(c) = Cl*max(l1,l2)
+      t_scale(c) = max(t1,t2)
+      l_scale(c) = c_l*max(l1,l2)
     end do
   end if
 

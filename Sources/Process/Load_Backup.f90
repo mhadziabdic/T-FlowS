c9af347542aa8cfa520d2f2d1c142ffaf6ea9248
Niceno <bojan@niceno.ch>
2018-05-12 14:02:49 +0200

    First really functional version of Load and Save backup functions.

--- Sources/Process/Load_Backup.f90
+++ Sources/Process/Load_Backup.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Load_Backup(grid, time_step)
+  subroutine Load_Backup(grid, time_step, restart)
 !------------------------------------------------------------------------------!
 !   Read backup files. name.backup                                             !
 !----------------------------------[Modules]-----------------------------------!
@@ -11,24 +11,31 @@
 ! use rans_mod
 ! use Tokenizer_Mod
   use Grid_Mod
-! use Control_Mod
+  use Control_Mod
+  use Work_Mod,    only: phi => r_cell_01
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
   type(Grid_Type) :: grid
   integer         :: time_step
+  logical         :: restart 
 !-----------------------------------[Locals]-----------------------------------!
-  character(len=80) :: name_out, vn
-  integer           :: fh, error, vs, disp, c
+  character(len=80) :: name_in, vn, answer
+  integer           :: fh, error, vs, disp, c, c1, c2, s
 !==============================================================================!
 
-  !------------------------!
-  !   Create backup file   !
-  !------------------------!
-  call Name_File(0, name_out, '.backup')
+  ! Full name is specified in control file
+  call Control_Mod_Load_Backup_Name(name_in)
+
+  answer = name_in
+  call To_Upper_Case(answer)
+  if(answer == 'SKIP') then
+    restart = .false.
+    return 
+  end if
 
   ! Open backup file
-  call Comm_Mod_Open_File_Read(fh, name_out)
+  call Comm_Mod_Open_File_Read(fh, name_in)
 
   ! Create new types
   call Comm_Mod_Create_New_Types()
@@ -40,17 +47,17 @@
   !-----------------------------------------------! 
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   if(vn == 'x_coordinate') disp = disp + vs
   
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   if(vn == 'y_coordinate') disp = disp + vs
 
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   if(vn == 'z_coordinate') disp = disp + vs
 
   !---------------!
@@ -59,86 +66,87 @@
   !               !
   !---------------! 
 
+  ! Time step
+  call Comm_Mod_Read_Text(fh, vn, disp)
+  call Comm_Mod_Read_Int (fh, vs, disp)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn), 'with size', vs
+  call Comm_Mod_Read_Int (fh, time_step, disp)
+
   ! Velocity 
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, u % n( 1: nc_s), disp)
-  call Comm_Mod_Read_Bnd_Real (fh, u % n(-nb_s:-1), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, v % n( 1: nc_s), disp)
-  call Comm_Mod_Read_Bnd_Real (fh, v % n(-nb_s:-1), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, w % n( 1: nc_s), disp)
+  call Comm_Mod_Read_Bnd_Real (fh, u % n(-nb_s:-1), disp)
+  call Comm_Mod_Read_Bnd_Real (fh, v % n(-nb_s:-1), disp)
   call Comm_Mod_Read_Bnd_Real (fh, w % n(-nb_s:-1), disp)
 
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, u % o( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, v % o( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, w % o( 1: nc_s), disp)
 
-
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, u % a( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, v % a( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, w % a( 1: nc_s), disp)
 
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, u % a_o( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, v % a_o( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, w % a_o( 1: nc_s), disp)
 
-
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, u % d_o( 1: nc_s), disp)
-
-  call Comm_Mod_Read_Text(fh, vn, disp)
-  call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
   call Comm_Mod_Read_Cell_Real(fh, v % d_o( 1: nc_s), disp)
+  call Comm_Mod_Read_Cell_Real(fh, w % d_o( 1: nc_s), disp)
 
+  ! Pressure
   call Comm_Mod_Read_Text(fh, vn, disp)
   call Comm_Mod_Read_Int (fh, vs, disp)
-  print *, '# Found variable: ', trim(vn)
-  call Comm_Mod_Read_Cell_Real(fh, w % d_o( 1: nc_s), disp)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
+  call Comm_Mod_Read_Cell_Real(fh, p  % n( 1: nc_s), disp)
+  call Comm_Mod_Read_Cell_Real(fh, pp % n( 1: nc_s), disp)
+  call Comm_Mod_Read_Bnd_Real (fh, p  % n(-nb_s:-1), disp)
+  call Comm_Mod_Read_Bnd_Real (fh, pp % n(-nb_s:-1), disp)
+
+  !----------------------------------------------------!
+  !   Mass flow rates (ask Egor if name is correct?)   !
+  !----------------------------------------------------!
+
+  ! Perform actual reading
+  call Comm_Mod_Read_Text(fh, vn, disp)
+  call Comm_Mod_Read_Int (fh, vs, disp)
+  if(this_proc < 2) print *, '# Found variable: ', trim(vn)
+  call Comm_Mod_Read_Face_Real(fh, flux( 1: nf_s + nbf_s), disp)
+
+  ! Fix signs for the face fluxes back
+  do c = 1, grid % n_cells
+    phi(c) = cell_map(c)  ! holds global cell numbah I trully hope
+  end do
+  call Comm_Mod_Exchange(grid, phi)
+  do s = nf_s + 1, nf_s + nbf_s
+    c1 = grid % faces_c(1,s)
+    c2 = grid % faces_c(2,s)
+    if(phi(c1) > phi(c2)) flux(s) = -flux(s)
+  end do
+
+!TEST  do s = 1, nf_s
+!TEST    print '(a6,i4.4,a4,f18.3)', ' flux(', face_map(s)+1, ') = ', flux(s)
+!TEST  end do
+!TEST  do s = 1, nbf_s
+!TEST    print '(a6,i4.4,a4,f18.3)', ' flux(', buf_face_map(buf_face_ord(s))+1, ') = ', flux(nf_s+s)
+!TEST  end do
 
   ! Close backup file
   call Comm_Mod_Close_File(fh)
--- Sources/Process/Save_Backup.f90
+++ Sources/Process/Save_Backup.f90
@@ -1,5 +1,5 @@
 !==============================================================================!
-  subroutine Save_Backup(grid, time_step)
+  subroutine Save_Backup(grid, time_step, name_save)
 !------------------------------------------------------------------------------!
 !   Writes backup files. name.backup                                          !
 !----------------------------------[Modules]-----------------------------------!
@@ -12,15 +12,21 @@
 ! use Tokenizer_Mod
   use Grid_Mod
 ! use Control_Mod
+  use Work_Mod,    only: phi => r_cell_01
 !------------------------------------------------------------------------------!
   implicit none
 !---------------------------------[Arguments]----------------------------------!
-  type(Grid_Type) :: grid
-  integer         :: time_step
+  type(Grid_Type)  :: grid
+  integer          :: time_step
+  character(len=*) :: name_save
 !-----------------------------------[Locals]-----------------------------------!
-  character(len=80) :: name_out, vn
-  integer           :: fh, error, vs, disp
+  character(len=80) :: name_out, store_name, vn
+  integer           :: fh, error, vs, disp, s, c1, c2, c
 !==============================================================================!
+
+  store_name = problem_name
+
+  problem_name = name_save
 
   ! Name backup file 
   call Name_File(0, name_out, '.backup')
@@ -36,18 +42,18 @@
   !-----------------------------------------------------------------------!
   !   Save cell-centre coordinates.  Could be useful for interpolations   !
   !-----------------------------------------------------------------------! 
-  vn = 'x_coordinate';     call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t + nb_t) * 8;  call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'x_coordinate';            call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = (nc_t + nb_t) * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, grid % xc( 1: nc_s), disp)
   call Comm_Mod_Write_Bnd_Real (fh, grid % xc(-nb_s:-1), disp)
 
-  vn = 'y_coordinate';     call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t + nb_t) * 8;  call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'y_coordinate';            call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = (nc_t + nb_t) * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, grid % yc( 1: nc_s), disp)
   call Comm_Mod_Write_Bnd_Real (fh, grid % yc(-nb_s:-1), disp)
 
-  vn = 'z_coordinate';     call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t + nb_t) * 8;  call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'z_coordinate';            call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = (nc_t + nb_t) * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, grid % zc( 1: nc_s), disp)
   call Comm_Mod_Write_Bnd_Real (fh, grid % zc(-nb_s:-1), disp)
 
@@ -57,75 +63,91 @@
   !               !
   !---------------! 
 
+  ! Time
+  vn = 'time_step'; call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = SIZE_INT;    call Comm_Mod_Write_Int (fh, vs, disp)
+  vs = time_step;   call Comm_Mod_Write_Int (fh, vs, disp)
+
   ! Velocity 
-  vn = 'u_velocity';      call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t + nb_t) * 8; call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'velocity';                    call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = 3 * (nc_t + nb_t) * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, u % n( 1: nc_s), disp)
-  call Comm_Mod_Write_Bnd_Real (fh, u % n(-nb_s:-1), disp)
-
-  vn = 'v_velocity';      call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t + nb_t) * 8; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, v % n( 1: nc_s), disp)
-  call Comm_Mod_Write_Bnd_Real (fh, v % n(-nb_s:-1), disp)
-
-  vn = 'w_velocity';      call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t + nb_t) * 8; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, w % n( 1: nc_s), disp)
+  call Comm_Mod_Write_Bnd_Real (fh, u % n(-nb_s:-1), disp)
+  call Comm_Mod_Write_Bnd_Real (fh, v % n(-nb_s:-1), disp)
   call Comm_Mod_Write_Bnd_Real (fh, w % n(-nb_s:-1), disp)
 
-
-  vn = 'u_velocity_old';  call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;        call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'velocity_old';       call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = 3 * nc_t * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, u % o( 1: nc_s), disp)
-
-  vn = 'v_velocity_old';  call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;        call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, v % o( 1: nc_s), disp)
-
-  vn = 'w_velocity_old';  call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;        call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, w % o( 1: nc_s), disp)
 
-
-  vn = 'u_velocity_advection'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;             call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'velocity_advection'; call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = 3 * nc_t * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, u % a( 1: nc_s), disp)
-
-  vn = 'v_velocity_advection'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;             call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, v % a( 1: nc_s), disp)
-
-  vn = 'w_velocity_advection'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;             call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, w % a( 1: nc_s), disp)
 
-  vn = 'u_velocity_advection_old'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;                 call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'velocity_advection_old'; call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = 3 * nc_t * SIZE_REAL;     call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, u % a_o( 1: nc_s), disp)
-
-  vn = 'v_velocity_advection_old'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;                 call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, v % a_o( 1: nc_s), disp)
-
-  vn = 'w_velocity_advection_old'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;                 call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, w % a_o( 1: nc_s), disp)
 
-
-  vn = 'u_velocity_diffusion_old'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;                 call Comm_Mod_Write_Int (fh, vs, disp)
+  vn = 'velocity_diffusion_old'; call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = 3 * nc_t * SIZE_REAL;     call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, u % d_o( 1: nc_s), disp)
-
-  vn = 'v_velocity_diffusion_old'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;                 call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, v % d_o( 1: nc_s), disp)
-
-  vn = 'w_velocity_diffusion_old'; call Comm_Mod_Write_Text(fh, vn, disp)
-  vs = (nc_t) * 8;                 call Comm_Mod_Write_Int (fh, vs, disp)
   call Comm_Mod_Write_Cell_Real(fh, w % d_o( 1: nc_s), disp)
 
+  ! Pressure
+  vn = 'pressure';                    call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = 2 * (nc_t + nb_t) * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
+  call Comm_Mod_Write_Cell_Real(fh, p  % n( 1: nc_s), disp)
+  call Comm_Mod_Write_Cell_Real(fh, pp % n( 1: nc_s), disp)
+  call Comm_Mod_Write_Bnd_Real (fh, p  % n(-nb_s:-1), disp)
+  call Comm_Mod_Write_Bnd_Real (fh, pp % n(-nb_s:-1), disp)
+
+  !----------------------------------------------------!
+  !   Mass flow rates (ask Egor if name is correct?)   !
+  !----------------------------------------------------!
+
+  ! Change the sign of flux where necessary
+  do c = 1, grid % n_cells
+    phi(c) = cell_map(c)  ! holds global cell numbah I trully hope
+  end do
+  call Comm_Mod_Exchange(grid, phi)
+  do s = nf_s+1, nf_s+nbf_s
+    c1 = grid % faces_c(1,s)
+    c2 = grid % faces_c(2,s)
+    if(phi(c1) > phi(c2)) flux(s) = -flux(s)
+  end do
+
+!TEST  ! Test face buffers
+!TEST  do s = 1, grid % n_faces
+!TEST    flux(s) = 1000000.0 * grid % xf(s) +  &
+!TEST                 1000.0 * grid % yf(s) +  &
+!TEST                    1.0 * grid % zf(s)
+!TEST    print '(a6,i4.4,a4,f18.3)', ' flux(', s, ') = ', flux(s)
+!TEST  end do
+
+  ! Perform the actual saving
+  vn = 'mass_flow_rate';   call Comm_Mod_Write_Text(fh, vn, disp)
+  vs = (nf_t) * SIZE_REAL; call Comm_Mod_Write_Int (fh, vs, disp)
+  call Comm_Mod_Write_Face_Real(fh, flux( 1: nf_s + nbf_s), disp)
+
+  ! Fix signs for the face fluxes back
+  do s = nf_s + 1, nf_s + nbf_s
+    c1 = grid % faces_c(1,s)
+    c2 = grid % faces_c(2,s)
+    if(phi(c1) > phi(c2)) flux(s) = -flux(s)
+  end do
 
   ! Close backup file
   call Comm_Mod_Close_File(fh)
+
+  problem_name = store_name
 
   end subroutine

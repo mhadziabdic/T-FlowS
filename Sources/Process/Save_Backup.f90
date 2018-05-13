2b5308c59120994654d08a7844b09f549287ab70
Niceno <bojan@niceno.ch>
2018-05-13 22:11:14 +0200

    Added some turbulence models to Save and Load backup functions.

--- Sources/Process/Save_Backup.f90
+++ Sources/Process/Save_Backup.f90
@@ -97,6 +97,61 @@
     call Write_Backup_1_Cell(fh, d, 'temp_cros_old', t  % c_o(1:nc_s))
   end if
 
+  !-----------------------!
+  !   Turbulence models   !
+  !-----------------------!
+  if(turbulence_model == K_EPS    .or.  &
+     turbulence_model == K_EPS_ZETA_F     .or.  &
+     turbulence_model == HYBRID_K_EPS_ZETA_F) then
+
+    ! K
+    call Write_Backup_1_Cell_Bnd(fh, d, 'kin',      kin % n(-nb_s:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'kin_old',      kin % o  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'kin_adv',      kin % a  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'kin_adv_old',  kin % a_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'kin_dif_old',  kin % d_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'kin_cros',     kin % c  (1:nc_s))
+
+    ! Eps
+    call Write_Backup_1_Cell_Bnd(fh, d, 'eps',      eps % n(-nb_s:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'eps_old',      eps % o  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'eps_adv',      eps % a  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'eps_adv_old',  eps % a_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'eps_dif_old',  eps % d_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'eps_cros',     eps % c  (1:nc_s))
+
+    ! Other turbulent quantities
+    call Write_Backup_1_Cell_Bnd(fh, d, 'p_kin',    p_kin   (-nb_s:nc_s))
+    call Write_Backup_1_Cell_Bnd(fh, d, 'u_tau',    u_tau   (-nb_s:nc_s))
+    call Write_Backup_1_Cell_Bnd(fh, d, 'y_plus',   y_plus  (-nb_s:nc_s))
+    call Write_Backup_1_Cell_Bnd(fh, d, 'vis_wall', vis_plus(-nb_s:nc_s))
+    call Write_Backup_1_Cell    (fh, d, 'tau_wall', tau_wall  (1:nc_s))
+  end if
+
+  if(turbulence_model == K_EPS_ZETA_F     .or.  &
+     turbulence_model == HYBRID_K_EPS_ZETA_F) then
+
+    ! Zeta
+    call Write_Backup_1_Cell_Bnd(fh, d, 'zeta',     zeta % n(-nb_s:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'zeta_old',     zeta % o  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'zeta_adv',     zeta % a  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'zeta_adv_old', zeta % a_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'zeta_dif_old', zeta % d_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'zeta_cros',    zeta % c  (1:nc_s))
+
+    ! F22
+    call Write_Backup_1_Cell_Bnd(fh, d, 'f22',      f22 % n(-nb_s:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'f22_old',      f22 % o  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'f22_adv',      f22 % a  (1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'f22_adv_old',  f22 % a_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'f22_dif_old',  f22 % d_o(1:nc_s))
+    call Write_Backup_1_Cell(fh, d, 'f22_cros',     f22 % c  (1:nc_s))
+
+    ! Time and lenght scale
+    call Write_Backup_1_Cell_Bnd(fh, d, 't_scale',  t_scale(-nb_s:nc_s))
+    call Write_Backup_1_Cell_Bnd(fh, d, 'l_scale',  l_scale(-nb_s:nc_s))
+  end if 
+
   ! Close backup file
   call Comm_Mod_Close_File(fh)
 

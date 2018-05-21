146025d45474b42737c99933f4762d4a96d45cc1
Niceno <bojan@niceno.ch>
2018-05-21 16:29:32 +0200

    Changed the names of user defined scalars from US01, US02, ... to C_01, C_02, ...  The US thing was looking too much like United States and, let's be clear, 95% of user defined scalars will be concentrations of one sort or another.

--- Sources/Process/User_Mod/Allocate.f90
+++ Sources/Process/User_Mod/Allocate.f90
@@ -25,7 +25,7 @@
   do n = 1, n_user_scalars
 
     ! Set variable name like this for the time being, later in control file
-    v_name = 'US00'
+    v_name = 'C_00'
     write(v_name(3:4),'(i2.2)') n
 
     ! Allocate memory for user scalar

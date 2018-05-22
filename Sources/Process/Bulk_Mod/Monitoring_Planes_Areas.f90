38c8106292deb04ba251eb5c63ab497658de5dc8
Egor <palkinev89@gmail.com>
2018-05-22 18:18:14 +0200

    fixed issue with c2>0 and checking bnd_type

--- Sources/Process/Bulk_Mod/Monitoring_Planes_Areas.f90
+++ Sources/Process/Bulk_Mod/Monitoring_Planes_Areas.f90
@@ -28,65 +28,73 @@
     do s = 1, grid % n_faces
       c1 = grid % faces_c(1,s)
       c2 = grid % faces_c(2,s)
-      if(c2 > 0 .or.  &
-         c2 < 0 .and. Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then
-        if( (grid % material(c1) == m) .and.  &
-            (grid % material(c1) == grid % material(c2)) ) then
-          xc1=grid % xc(c1)
-          yc1=grid % yc(c1)
-          zc1=grid % zc(c1)
-          xc2=grid % xc(c1) + grid % dx(s)
-          yc2=grid % yc(c1) + grid % dy(s)
-          zc2=grid % zc(c1) + grid % dz(s)
- 
-          ax_t = abs(grid % sx(s))
-          ay_t = abs(grid % sy(s))
-          az_t = abs(grid % sz(s))
+      if(c2  < 0) then
+        if (Grid_Mod_Bnd_Cond_Type(grid, c2) .ne. BUFFER) then
+          cycle ! skip current surface
+        end if
+      end if
+      if( (grid % material(c1) == m) .and.  &
+          (grid % material(c1) == grid % material(c2)) ) then
+        xc1=grid % xc(c1)
+        yc1=grid % yc(c1)
+        zc1=grid % zc(c1)
+        xc2=grid % xc(c1) + grid % dx(s)
+        yc2=grid % yc(c1) + grid % dy(s)
+        zc2=grid % zc(c1) + grid % dz(s)
 
-          !-------!
-          !   x   !
-          !-------!
-          if( (xc1 <= bulk(m) % xp).and.(xc2 > bulk(m) % xp) .or. & 
-              (xc2 <= bulk(m) % xp).and.(xc1 > bulk(m) % xp) ) then
+        ax_t = abs(grid % sx(s))
+        ay_t = abs(grid % sy(s))
+        az_t = abs(grid % sz(s))
 
-            ! Watch out: buffer cell faces will be counted twice
-            if(c2  < 0 .and. Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then 
-              bulk(m) % area_x = bulk(m) % area_x + 0.5 * ax_t    
-            else
-              bulk(m) % area_x = bulk(m) % area_x + ax_t    
+        !-------!
+        !   x   !
+        !-------!
+        if( (xc1 <= bulk(m) % xp).and.(xc2 > bulk(m) % xp) .or. & 
+            (xc2 <= bulk(m) % xp).and.(xc1 > bulk(m) % xp) ) then
+
+          ! Watch out: buffer cell faces will be counted twice
+          if(c2  < 0) then
+            if (Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then
+              bulk(m) % area_x = bulk(m) % area_x + 0.5 * ax_t
             end if
+          else
+            bulk(m) % area_x = bulk(m) % area_x + ax_t    
           end if
+        end if
 
-          !-------!
-          !   y   !
-          !-------!
-          if( (yc1 <= bulk(m) % yp).and.(yc2 > bulk(m) % yp) .or. & 
-              (yc2 <= bulk(m) % yp).and.(yc1 > bulk(m) % yp) ) then
+        !-------!
+        !   y   !
+        !-------!
+        if( (yc1 <= bulk(m) % yp).and.(yc2 > bulk(m) % yp) .or. & 
+            (yc2 <= bulk(m) % yp).and.(yc1 > bulk(m) % yp) ) then
 
-            ! Watch out: buffer cell faces will be counted twice
-            if(c2  < 0 .and. Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then 
-              bulk(m) % area_y = bulk(m) % area_y + 0.5 * ay_t    
-            else
-              bulk(m) % area_y = bulk(m) % area_y + ay_t    
+          ! Watch out: buffer cell faces will be counted twice
+          if(c2  < 0) then
+            if (Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then
+              bulk(m) % area_y = bulk(m) % area_y + 0.5 * ay_t
             end if
+          else
+            bulk(m) % area_y = bulk(m) % area_y + ay_t    
           end if
+        end if
 
-          !-------!
-          !   z   !
-          !-------!
-          if( (zc1 <= bulk(m) % zp).and.(zc2 > bulk(m) % zp) .or. & 
-              (zc2 <= bulk(m) % zp).and.(zc1 > bulk(m) % zp) ) then
+        !-------!
+        !   z   !
+        !-------!
+        if( (zc1 <= bulk(m) % zp).and.(zc2 > bulk(m) % zp) .or. & 
+            (zc2 <= bulk(m) % zp).and.(zc1 > bulk(m) % zp) ) then
 
-            ! Watch out: buffer cell faces will be counted twice
-            if(c2  < 0 .and. Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then 
-              bulk(m) % area_z = bulk(m) % area_z + 0.5 * az_t    
-            else
-              bulk(m) % area_z = bulk(m) % area_z + az_t    
+          ! Watch out: buffer cell faces will be counted twice
+          if(c2  < 0) then
+            if (Grid_Mod_Bnd_Cond_Type(grid, c2) == BUFFER) then
+              bulk(m) % area_z = bulk(m) % area_z + 0.5 * az_t
             end if
+          else
+            bulk(m) % area_z = bulk(m) % area_z + az_t    
           end if
+        end if
 
-        end if  
-      end if  ! (c2 > 0 .and. ... )
+      end if
     end do
 
     call Comm_Mod_Global_Sum_Real(bulk(m) % area_x)
